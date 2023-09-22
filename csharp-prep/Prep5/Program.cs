eef94be39c169b22a8f72d11255033d9b077e551
rafitos123 <rafael.antunes2705@gmail.com>
2023-09-22 00:45:07 -0300

    square

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayMessage();
+        string userName = EnterUserName();
+        int userNumber = EnterUserNumber();
+
+        int NumberSquare = SquareNumber(userNumber);
+
+        DisplayResult(userName, NumberSquare);
     }
+        static void DisplayMessage()
+            {
+                Console.WriteLine("Welcome to the program!");
+            }
+        
+        static string EnterUserName()
+        {
+
+            Console.Write("What is your name? ");
+            string my_name = Console.ReadLine();
+            return my_name;
+
+        }
+
+        static int EnterUserNumber()
+        {
+            Console.Write("Enter a number");
+            int my_number = int.Parse(Console.ReadLine());
+            return my_number;
+        }
+
+        static int SquareNumber(int number)
+        {
+            int square = number * number;
+            return square;
+        }
+
+         static void DisplayResult(string name, int square)
+        {
+            Console.WriteLine($"Hi {name}, the square of your number is {square}");
+        }
 }

dee0c8cabae8787aab0d276ec34ce93167ff331d
rafitos123 <rafael.antunes2705@gmail.com>
2023-09-21 23:54:05 -0300

    guesse

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Globalization;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {   
+        Random randomNumber = new Random();
+        int number = randomNumber.Next(1,10);
+        int my_guess = 0;
+
+        while (my_guess != number)
+        {
+            Console.Write("What is your guess? ");
+            my_guess = int.Parse(Console.ReadLine());
+            if (my_guess < number)
+            {
+                Console.WriteLine("Higher");
+                
+            }
+            else if (my_guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }

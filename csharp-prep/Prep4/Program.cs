ed14d97f892734061d3ce8fd07fb0196841a820d
rafitos123 <rafael.antunes2705@gmail.com>
2023-09-22 00:20:35 -0300

    list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
+    {   
+        List<int> numbers = new List<int>();
+        int item = -1;
+    while (item != 0)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
+        item = int.Parse(Console.ReadLine());
+        if (item != 0)
+        {
+            numbers.Add(item);
+        }
+   
+    }
+      int sum = 0;
+      foreach (int number in numbers )
+      {
+        sum += number;
+      }
+      Console.WriteLine($"The sum is: {sum}");
     }
 }

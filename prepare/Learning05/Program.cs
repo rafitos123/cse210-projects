c5a272a5f12e600e7b8b0e4d11010c6b7a7a6236
rafitos123 <rafael.antunes2705@gmail.com>
2023-11-09 14:28:02 -0300

    shape area

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square test1 = new Square("blue", 4);
+        shapes.Add(test1);
+
+        Rectangle test2 = new Rectangle("red", 4, 2);
+        shapes.Add(test2);
+        
+        Circle test3 = new Circle("green", 6);
+        shapes.Add(test3);
+
+        foreach(Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+
+            Console.WriteLine($"Color: {color} - Shape area: {area}");
+
+
+        }
     }
 }
---
+++ prepare/Learning05/shape.cs
@@ -0,0 +1,19 @@
+using System;
+
+public abstract class Shape
+{
+    protected string _color; 
+
+    public Shape(string color)
+    {
+        _color = color;
+    }
+
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    public abstract double GetArea();
+    
+}
